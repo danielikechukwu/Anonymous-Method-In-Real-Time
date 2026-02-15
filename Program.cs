f05b6a0c3d144ec5b443f4596f79135e9b346c8d
Daniel Ikechukwu <Ikechukwudaniel080@gmail.com>
2026-02-15 11:41:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnonymousMethodInRealTime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
