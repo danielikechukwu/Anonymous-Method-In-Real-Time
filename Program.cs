204d061541cd3434d33458891a7d237697beab5c
Daniel Ikechukwu <Ikechukwudaniel080@gmail.com>
2026-02-15 14:49:47 +0100

    Feat(lambda): Implement both anonymous function and lambda expression both giving same result.

--- Program.cs
+++ Program.cs
@@ -21,12 +21,34 @@ namespace AnonymousMethodInRealTime
                 new Employee{ ID = 104, Name = "Sambit", Gender = "Male", Salary = 500000},
             };
 
-            Employee employee = listEmployees.Find(x => employeePredicate(x));
+            //Employee employee = listEmployees.Find(x => employeePredicate(x));
+
+            /***
+             * Lambda expression
+             */
+            Employee employee = listEmployees.Find((Employee x) => {
+                return x.ID == 103;
+            });
+
+            /***
+             * Anonymous function
+             */
+            Employee employee1 = listEmployees.Find(delegate (Employee x) {
+                return x.ID == 103;
+            });
 
             Console.WriteLine(@"ID : {0}, Name : {1}, Gender : {2}, Salary : {3}",
                 employee.ID, employee.Name, employee.Gender, employee.Salary);
 
+            Console.WriteLine(@"ID : {0}, Name : {1}, Gender : {2}, Salary : {3}",
+                employee1.ID, employee1.Name, employee1.Gender, employee1.Salary);
+
             Console.ReadKey();
+
+            /***
+             * Both the Anonymous function and lambda expression provide same result
+             * Lambda expression are basically anonymous function used to create delegate
+             * **/
         }
 
         public static bool IsEmployeeExist(Employee emp)
